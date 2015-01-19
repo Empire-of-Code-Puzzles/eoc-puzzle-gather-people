f695a09a6a3a42ef484ea5075d3c4c6a93e59c7a
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-01-19 10:42:33 +0300

    fix referee

--- verification/referee.py
+++ verification/referee.py
@@ -42,7 +42,8 @@ api.add_listener(
             'python-27': cover_codes.unwrap_args,  # or None
             'python-3': cover_codes.unwrap_args
         },
-        function_name="golf"
+        function_name="golf",
+        max_length="250"
         # checker=None,  # checkers.float.comparison(2)
         # add_allowed_modules=[],
         # add_close_builtins=[],
